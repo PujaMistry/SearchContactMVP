6f4b49b7a6e6ff379e5fad3133b8d7b75f83126e
Puja <pujamistry33@gmail.com>
2018-09-07 15:13:46 -0400

    first commit

--- app/src/main/java/com/example/puja/searchcontactmvp/MainActivity.java
+++ app/src/main/java/com/example/puja/searchcontactmvp/MainActivity.java
@@ -38,6 +38,8 @@ public class MainActivity extends AppCompatActivity implements MainView, Contact
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+
         setContentView(R.layout.activity_main);
         unbinder = ButterKnife.bind(this);
 
